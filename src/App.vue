c83d2f51135c737e2aadbd42b2f642c5e80a1eb9
Asia Lie <asia.lie@softwire.com>
2024-11-20 11:27:59 +0000

    Add Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
 
 </script>
 
 <template>
 
 <NavBar />
+<Hero />
+
 </template>
