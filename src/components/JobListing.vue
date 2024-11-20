b211d997ccefa9a4b1271ca2cbbe406fdbe4db65
Asia Lie <asia.lie@softwire.com>
2024-11-20 14:09:20 +0000

    Add routerlinks

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { defineProps, ref, computed } from 'vue';
 import type { Job } from '@/interfaces/Job.ts';
+import { RouterLink } from 'vue-router';
 
 const props = defineProps<{job: Job;}>();
 
@@ -46,12 +47,12 @@ const toggleFullDescription = () => {
                   <i class="pi pi-map-marker text-orange-700"></i>
                   {{job.location}}
                 </div>
-                <a
-                  :href="`/job/${job.id}`"
+                <RouterLink
+                  :to = "`/jobs/${job.id}`"
                   class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                   Read More
-                </a>
+                </RouterLink>
               </div>
             </div>
           </div>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,6 +2,7 @@
 import jobData from '@/jobs.json'
 import { ref, defineProps } from 'vue';
 import JobListing from '@/components/JobListing.vue';
+import { RouterLink } from 'vue-router';
 
 const jobs = ref(jobData);
 
@@ -28,10 +29,10 @@ defineProps({
 </section>
 
 <section v-if = "showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
 
